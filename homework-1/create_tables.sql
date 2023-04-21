975b54c158fb49cbe2b2c99da233448f99f0d41e
wannagetout <inv.moiseev@gmail.com>
2023-04-21 08:24:50 +0300

    hw-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,4 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(employee_id SERIAL PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(100),title VARCHAR(100),birth_date DATE,notes text);
+CREATE TABLE customers(customer_id VARCHAR(50) PRIMARY KEY, company_name VARCHAR(100), contact_name VARCHAR(100));
+CREATE TABLE orders(order_id INT PRIMARY KEY, customer_id varchar(100), employee_id INT REFERENCES employees.employee_id, order_date VARCHAR(100), ship_city VARCHAR(100));
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,19 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+if __name__ == '__main__':
+    with psycopg2.connect(host='localhost', database='north', user='postgres', password='postgres') as conn:
+        cur = conn.cursor()
+        with open('north_data/employees_data.csv', 'r', encoding='utf-8') as csv_file:
+            cur.copy_from(csv_file, 'employees', sep=',')
+
+        with open('north_data/customers_data.csv', 'r') as csv_file:
+            cur.copy_from(csv_file, 'customers', sep=',')
+            csv_file.close()
+
+        with open('north_data/orders_data.csv', 'r') as csv_file:
+            cur.copy_from(csv_file, 'orders', sep=',')
+            csv_file.close()
+
+    conn.commit()
+    cur.close()
