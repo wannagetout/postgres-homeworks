115a5e12bd3fd347da58a56adc9e6e82fcd32ed3
wannagetout <inv.moiseev@gmail.com>
2023-04-22 12:18:51 +0300

    hw-3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,35 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) AS ФИО
+FROM orders
+JOIN customers USING (customer_id)
+JOIN employees USING (employee_id)
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city='London' AND employees.city='London' AND shippers.company_name='United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products
+JOIN suppliers USING (supplier_id)
+JOIN categories USING (category_id)
+WHERE discontinued = 0 AND units_in_stock < 25 and category_name in('Dairy Products', 'Condiments')
+ORDER BY units_in_stock DESC
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name
+FROM customers
+LEFT JOIN orders USING (customer_id)
+WHERE order_id is null
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT product_name
+FROM products
+WHERE EXISTS (SELECT * FROM order_details WHERE units_in_stock=10)
